07a6d2d1013b6a3d0a24801660148979010c2b90
WallysonDuarte <wallyson.duarte@gmail.com>
2026-05-04 20:20:09 -0300

    Corrige deploy.sh, atualiza favicon CB e BaseLayout async
    - deploy.sh: REMOTE_DIR corrigido para /opt/blogdudu
    - favicon.svg/png/ico: novo logo CB geométrico (triângulos coloridos)
    - BaseLayout: fonts e Font Awesome async (media=print onload)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-REMOTE_DIR="/opt/curitibablog"
+REMOTE_DIR="/opt/blogdudu"
 SSH="ssh -i ~/.ssh/id_server_nopass -p 2222 ubuntu@31.97.252.45"
 SCP="scp -i ~/.ssh/id_server_nopass -P 2222"
 
@@ -9,19 +9,19 @@ echo "==> Build..."
 npm run build
 
 echo "==> Compactando dist..."
-tar -czf /tmp/curitibablog.tar.gz -C dist .
+tar -czf /tmp/blogdudu.tar.gz -C dist .
 
 echo "==> Enviando para servidor..."
-$SCP /tmp/curitibablog.tar.gz ubuntu@31.97.252.45:/tmp/
+$SCP /tmp/blogdudu.tar.gz ubuntu@31.97.252.45:/tmp/
 
 echo "==> Deploy no servidor..."
 $SSH "
   sudo rm -rf $REMOTE_DIR/*
-  sudo tar -xzf /tmp/curitibablog.tar.gz -C $REMOTE_DIR
+  sudo tar -xzf /tmp/blogdudu.tar.gz -C $REMOTE_DIR
   sudo chown -R www-data:www-data $REMOTE_DIR
-  rm /tmp/curitibablog.tar.gz
+  rm /tmp/blogdudu.tar.gz
   echo 'Deploy concluído!'
 "
 
-rm /tmp/curitibablog.tar.gz
-echo "==> CuritibaBlog deployado em curitibablog.com.br"
+rm /tmp/blogdudu.tar.gz
+echo "==> BlogDudu deployado em blogdudu.com.br"
